652d4006d7cc2444a1cfc7f6856b0cea712917b5
shaistha-samreen <shaisthasamreen786@gmail.com>
2024-01-25 06:43:49 -0500

    my first component of big basket

---
+++ app/components/header.js
@@ -0,0 +1,20 @@
+
+const Header = () => {
+    return (
+        <main>
+      <header>
+        <div className="flex bg-green-800 justify-center py-1 text-white ">
+
+          <div className="font-bold items-center">Delivery Address-----------
+          <p className="text-xs font-thin items-center">Home - 54652 1255 Siddiq Nagar, HITEC City</p></div>
+            <div className="font-bold">Delivery Options-----------
+            <p className="text-xs font-thin">Choose your convenient date and time for delivery</p></div>
+            <div className="font-bold">Payment Options
+            <p className="text-xs font-thin">Pay Order amount by selecting any payment mode</p></div>
+        </div>
+      </header>
+      </main>
+    )
+  };
+  
+  export default Header;
